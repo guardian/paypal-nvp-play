a7ccd672f504564ba12e1247ac66fff5e3acad8e
Jamie Byers <james.byers@guardian.co.uk>
2016-12-02 16:16:41 +0000

    Added webpack, and reworked js assets to use it.

--- app/assets/javascripts/paypal.js
+++ app/assets/javascripts/paypal.js
@@ -1,15 +1,19 @@
-paypal.Button.render({
+export function init () {
+
+	paypal.Button.render({
 				
-	env: 'sandbox', // Specify 'production' for the prod environment
+		env: 'sandbox', // Specify 'production' for the prod environment
 
-	// Called when user clicks Paypal button.
-	payment: function (resolve, reject) {
-		console.log('Setting up payment.')
-	},
+		// Called when user clicks Paypal button.
+		payment: function (resolve, reject) {
+			console.log('Setting up payment.')
+		},
 
-	// Called when user finishes with Paypal interface (approves payment).
-	onAuthorize: function (data, actions) {
-		console.log('Authorised.');
-   }
-		
-}, '#paypal-button');
+		// Called when user finishes with Paypal interface (approves payment).
+		onAuthorize: function (data, actions) {
+			console.log('Authorised.');
+	   }
+			
+	}, '#paypal-button');
+
+};
