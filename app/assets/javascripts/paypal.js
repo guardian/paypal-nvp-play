b8d988a847373c8081c378d75c1672aec88807ed
Jamie Byers <james.byers@guardian.co.uk>
2016-12-02 16:04:02 +0000

    Added webpack as dep, and included paypal script to handle payments client-side.

---
+++ app/assets/javascripts/paypal.js
@@ -0,0 +1,15 @@
+paypal.Button.render({
+				
+	env: 'sandbox', // Specify 'production' for the prod environment
+
+	// Called when user clicks Paypal button.
+	payment: function (resolve, reject) {
+		console.log('Setting up payment.')
+	},
+
+	// Called when user finishes with Paypal interface (approves payment).
+	onAuthorize: function (data, actions) {
+		console.log('Authorised.');
+   }
+		
+}, '#paypal-button');
