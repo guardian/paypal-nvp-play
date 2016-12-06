64ca28f71697f396298de13d4a35e24a9a7cbfae
Jamie Byers <james.byers@guardian.co.uk>
2016-12-06 11:26:50 +0000

    Filled out setupPayment function with Paypal requests.

--- app/configuration/Config.scala
+++ app/configuration/Config.scala
@@ -3,8 +3,9 @@ package configuration
 object Config {
 
 	val paypalSandboxUrl = "https://api-3t.sandbox.paypal.com/nvp"
-	val paypalUser = sys.env.get("PAYPAL_SANDBOX_USER")
-	val paypalPassword = sys.env.get("PAYPAL_SANDBOX_PWD")
-	val paypalSignature = sys.env.get("PAYPAL_SANDBOX_SIG")
+	val paypalUser = sys.env.get("PAYPAL_SANDBOX_USER").get
+	val paypalPassword = sys.env.get("PAYPAL_SANDBOX_PWD").get
+	val paypalSignature = sys.env.get("PAYPAL_SANDBOX_SIG").get
+	val paypalNVPVersion = "124.0"
 
 }
--- app/controllers/Paypal.scala
+++ app/controllers/Paypal.scala
@@ -1,15 +1,38 @@
 package controllers
 
-import play.api.mvc.{Controller}
-import okhttp3.Request
+import play.api.mvc.{Controller, Action}
+import okhttp3.{OkHttpClient, FormBody, Request}
 import configuration.Config
 
 class Paypal extends Controller {
 
 	def setupPayment (amount: String,
-		currency: String,
-		returnUrl: String,
-		cancelUrl: String) = TODO
+		currency: String) = Action {
+
+		val client = new OkHttpClient()
+		val requestBody = new FormBody.Builder()
+			.add("USER", Config.paypalUser)
+			.add("PWD", Config.paypalPassword)
+			.add("SIGNATURE", Config.paypalSignature)
+			.add("VERSION", Config.paypalNVPVersion)
+			.add("METHOD", "SetExpressCheckout")
+			.add("PAYMENTREQUEST_0_PAYMENTACTION", "SALE")
+			.add("PAYMENTREQUEST_0_AMT", amount)
+			.add("PAYMENTREQUEST_0_CURRENCYCODE", currency)
+			.add("RETURNURL", "http://localhost:5000/create-agreement")
+			.add("CANCELURL", "http://localhost:5000/cancel")
+			.add("BILLINGTYPE", "MerchantInitiatedBilling")
+			.build()
+
+		val request = new Request.Builder()
+			.url(Config.paypalSandboxUrl)
+			.post(requestBody)
+			.build()
+
+		val response = client.newCall(request).execute()
+		Ok(response.body())
+
+	}
 
 	def createAgreement (token: String) = TODO
 
